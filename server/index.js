404374e1c5d357f151533d233d9a2479fbdf783b
fredivazquez-UPTap <92467531+fredivazquez-UPTap@users.noreply.github.com>
2021-10-14 14:23:10 -0500

    Add nodemon

--- server/index.js
+++ server/index.js
@@ -1,23 +1,47 @@
 const express = require("express");
+const { Client } = require("pg");
+
 const app = express();
-const port = 2500;
-const pg = require("pg");
+const port = 3000;
 
-var connectionString = "pg://postgres:postgres@localhost:5432/db";
-var client = new pg.Client(connectionString);
+const connectionData = {
+  user: "postgres",
+  host: "localhost",
+  database: "pern",
+  password: "1234567890",
+  port: 5432,
+};
+const client = new Client(connectionData);
+var users = {};
 client.connect();
-
-var queryString = "SELECT * FROM students";
-var query = client.query(queryString);
-
-query.on("row", (row, result) => {
-  result.addRow(row);
-});
+client
+  .query("SELECT * FROM users")
+  .then((response) => {
+    console.log(response.rows);
+    users = response.rows;
+    client.end();
+  })
+  .catch((err) => {
+    console.log(err);
+    client.end();
+  });
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send(`Usuarios: ${users}`);
+});
+
+app.post("/", function (req, res) {
+  res.send("Got a POST request");
+});
+
+app.put("/user", function (req, res) {
+  res.send("Got a PUT request at /user");
+});
+
+app.delete("/user", function (req, res) {
+  res.send("Got a DELETE request at /user");
 });
 
 app.listen(port, () => {
-  console.log(`Conexión exitosa.`);
+  console.log(`Example app listening at http://localhost:${port}`);
 });
